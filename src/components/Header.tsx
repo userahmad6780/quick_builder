20e68142f536876f3f03911b0cc20d327c97ff24
userahmad6780 <mahmadctn38@gmail.com>
2025-04-28 12:28:44 +0500

    style(button): updated button style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ function Header() {
             <h1 className="text-2xl md:text-5xl font-semibold mb-5 md:mb-12">Write your story with the <br/> ultimate resume builder</h1>
             <span className="text-md md:text-lg">Only 2% of resumes win. Yours will be one of them. Let´s build<br/>  you a resume that works.</span>
             <div className="mt-2">
-                <Button className='text-sm lg:text-xl font-medium py-4 px-2 md:py-6 md:px-3 bg-blue-500 hover:bg-blue-300'>Create my resume</Button>
+                <Button className='text-sm lg:text-xl font-medium py-4 px-2 md:py-6 md:px-3 text-white bg-blue-500 hover:bg-blue-300'>Create my resume</Button>
             </div>
             <div className="flex gap-10 mt-16">
                 <div className="flex flex-col gap-2 border-l border-gray-400 pl-3">
